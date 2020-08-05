db437644ce5c9a5a7217422c69188a5815aa4946
Eddie Jaoude <eddie@jaoudestudios.com>
2020-08-05 23:03:55 +0100

    feat(update): replace readme with new content

--- index.js
+++ index.js
@@ -7,22 +7,19 @@ const readmeBox = require('readme-box').ReadmeBox;
 (async () => {
     try {
         const githubToken = core.getInput('github-token');
-        const filePath = core.getInput('json-file-path');
-        const content = fs.readFileSync(path.join(process.env.GITHUB_WORKSPACE, filePath));
+        const filePath = path.join(process.env.GITHUB_WORKSPACE, core.getInput('json-file-path'));
+        const content = fs.readFileSync(filePath);
         const json = JSON.parse(content);
-        console.log(json); // @TODO: debug
 
         const oldContent = fs.readFileSync(path.join(process.env.GITHUB_WORKSPACE, filePath), 'utf8');
-        const box = new ReadmeBox();
-        box.replaceSection({
+        const box = new readmeBox();
+        const replacedContents = box.replaceSection({
             section: 'data-section',
             oldContent,
-            newContent: content,
-            token: githubToken
+            newContent: content
         });
 
-        const time = new Date().toTimeString();
-        core.setOutput('time', time);
+        fs.writeFileSync(filePath, replacedContents);
     } catch (error) {
         core.setFailed(error.message);
     }
