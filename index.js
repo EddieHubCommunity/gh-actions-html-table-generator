1fe86657e1131e0bb753249e0a2d514e997bce0d
Eddie Jaoude <eddie@jaoudestudios.com>
2020-08-05 17:56:19 +0100

    fix(write): content should be a string

--- index.js
+++ index.js
@@ -1,27 +1,27 @@
-const core = require("@actions/core");
-const github = require("@actions/github");
-const fs = require("fs");
-const path = require("path");
-const readmeBox = require("readme-box").ReadmeBox;
+const core = require('@actions/core');
+const github = require('@actions/github');
+const fs = require('fs');
+const path = require('path');
+const readmeBox = require('readme-box').ReadmeBox;
 
 (async function () {
     try {
-        const filePath = core.getInput("json-file-path");
+        const filePath = core.getInput('json-file-path');
         const content = fs.readFileSync(
             path.join(process.env.GITHUB_WORKSPACE, filePath)
         );
         const json = JSON.parse(content);
         console.log(json); // @TODO: debug
 
-        await readmeBox.updateSection(json, {
-            owner: process.env.GITHUB_REPOSITORY.split("/")[0],
-            repo: process.env.GITHUB_REPOSITORY.split("/")[1],
+        await readmeBox.updateSection(content, {
+            owner: process.env.GITHUB_REPOSITORY.split('/')[0],
+            repo: process.env.GITHUB_REPOSITORY.split('/')[1],
             token: process.env.GITHUB_TOKEN,
-            section: "data-section",
+            section: 'data-section',
         });
 
         const time = new Date().toTimeString();
-        core.setOutput("time", time);
+        core.setOutput('time', time);
     } catch (error) {
         core.setFailed(error.message);
     }
