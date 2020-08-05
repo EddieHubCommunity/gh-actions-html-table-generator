322eb38ff9b9bec7b186e5397663a170a22cbd76
Eddie Jaoude <eddie@jaoudestudios.com>
2020-08-05 17:47:31 +0100

    feat(write): update the readme with json

--- index.js
+++ index.js
@@ -2,17 +2,23 @@ const core = require('@actions/core');
 const github = require('@actions/github');
 const fs = require('fs');
 const path = require('path');
+const readmeBox = require('readme-box').ReadmeBox;
 
 try {
     const filePath = core.getInput('json-file-path');
     const content = fs.readFileSync(path.join(process.env.GITHUB_WORKSPACE, filePath));
-    console.log(content);
-    console.log(JSON.parse(content));
+    const json = JSON.parse(content);
+    console.log(json); // @TODO: debug
+
+    await readmeBox.updateSection(json, {
+        owner: process.env.GITHUB_REPOSITORY.split('/')[0],
+        repo: process.env.GITHUB_REPOSITORY.split('/')[1],
+        token: process.env.GITHUB_TOKEN,
+        section: 'data-section'
+    });
+
     const time = (new Date()).toTimeString();
-    core.setOutput("time", time);
-    // Get the JSON webhook payload for the event that triggered the workflow
-    const payload = JSON.stringify(github.context.payload, undefined, 2)
-    console.log(`The event payload: ${payload}`);
+    core.setOutput('time', time);
 } catch (error) {
     core.setFailed(error.message);
 }
