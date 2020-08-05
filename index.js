d3bf65eb2c5f94f109af666d7087b9563eb13e6e
Eddie Jaoude <eddie@jaoudestudios.com>
2020-08-05 23:48:10 +0100

    fix(action): fixed branch ref

--- index.js
+++ index.js
@@ -10,12 +10,12 @@ const readmeBox = require('readme-box').ReadmeBox;
         const filePath = path.join(process.env.GITHUB_WORKSPACE, core.getInput('json-file-path'));
         const data = fs.readFileSync(filePath, 'utf8');
         // const json = JSON.parse(data);
-        console.log('GITHUB REF: ', process.env.GITHUB_REF);
+        console.log('GITHUB REF: ', process.env.GITHUB_REF.split('/')[2]);
 
         await readmeBox.updateSection(data, {
             owner: process.env.GITHUB_REPOSITORY.split('/')[0],
             repo: process.env.GITHUB_REPOSITORY.split('/')[1],
-            branch: process.env.GITHUB_REF,
+            branch: process.env.GITHUB_REF.split('/')[2],
             token: githubToken,
             section: 'data-section',
         });
