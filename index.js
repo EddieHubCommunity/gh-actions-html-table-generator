437998c8c1d153b1e9b76642eece00827ed4fb20
Eddie Jaoude <eddie@jaoudestudios.com>
2020-08-17 09:50:40 +0100

    chore(html): refactored code into functions

--- index.js
+++ index.js
@@ -5,30 +5,35 @@ const path = require('path');
 const readmeBox = require('readme-box').ReadmeBox;
 const chunk = require('chunk');
 
+const generateCell = (user) => {
+    return `<td align="center">
+        <p><a href="https://github.com/${user.githubUsername}">${user.name}</a></p>
+        <img src="${user.imageUrl}" />
+        <p><a href="https://github.com/EddieJaoudeCommunity/awesome-github-profiles/issues/${user.issueNumber}">(:100: give your vote)</a></p>
+    </td>`;
+}
+
+const generateRow = (row) => {
+    const cells = row.map((user) => generateCell(user));
+
+    if (cells.length < columns) {
+        cells.push('<td></td>'.repeat(columns - cells.length));
+    }
+
+    return `<tr>${cells.join('')}</tr>`;
+};
+
 (async () => {
+    const githubToken = core.getInput('github-token');
+    const filePath = path.join(process.env.GITHUB_WORKSPACE, core.getInput('json-file-path'));
+    const columns = core.getInput('columns');
+    const data = fs.readFileSync(filePath, 'utf8');
+    const json = JSON.parse(data);
+
+    let content = chunk(json, columns)
+        .map((row) => generateRow(row));
+
     try {
-        const githubToken = core.getInput('github-token');
-        const filePath = path.join(process.env.GITHUB_WORKSPACE, core.getInput('json-file-path'));
-        const columns = core.getInput('columns');
-        const data = fs.readFileSync(filePath, 'utf8');
-        const json = JSON.parse(data);
-        console.log('GITHUB REF: ', process.env.GITHUB_REF.split('/')[2]);
-
-        let content = chunk(json, columns)
-            .map((row) => {
-                const cells = row.map((user) => (`<td align="center">
-                            <p><a href="https://github.com/${user.githubUsername}">${user.name}</a></p>
-                            <img src="${user.imageUrl}" />
-                            <p><a href="https://github.com/EddieJaoudeCommunity/awesome-github-profiles/issues/${user.issueNumber}">(:100: give your vote)</a></p>
-                        </td>`));
-
-                if (cells.length < columns) {
-                    cells.push('<td></td>'.repeat(columns - cells.length));
-                }
-
-                return `<tr>${cells.join('')}</tr>`;
-        });
-
         await readmeBox.updateSection(`<table width="100%">${content.join('')}</table>`, {
             owner: process.env.GITHUB_REPOSITORY.split('/')[0],
             repo: process.env.GITHUB_REPOSITORY.split('/')[1],
