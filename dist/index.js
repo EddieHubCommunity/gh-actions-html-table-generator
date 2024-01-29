c21bad604d8db6ddf00497ae2af95238de07b170
Guilherme Branco Stracini <guilherme@guilhermebranco.com.br>
2024-01-29 20:47:11 +0000

    Update index.js

--- dist/index.js
+++ dist/index.js
@@ -3690,30 +3690,29 @@ function removeHook (state, name, method) {
 /***/ 3886:
 /***/ (function(module, exports) {
 
-"use strict";
 
 
 (function () {
 
     function chunk (collection, size) {
-    
+
         var result = [];
-        
+
         // default size to two item
         size = parseInt(size) || 2;
-        
+
         // add each chunk to the result
         for (var x = 0; x < Math.ceil(collection.length / size); x++) {
-            
+
             var start = x * size;
             var end = start + size;
-            
+
             result.push(collection.slice(start, end));
-            
+
         }
-        
+
         return result;
-        
+
     };
 
     // export in node or browser
